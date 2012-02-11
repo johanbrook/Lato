f0e81472e04e9fe22e8423f712b330ece4b6ead0
Johan Brook <johan.jvb@gmail.com>
2012-02-11 23:20:32 +0100

    Added prefs for dev/production mode

--- config/initializers/wordless_preferences.php
+++ config/initializers/wordless_preferences.php
@@ -4,12 +4,13 @@
  * Configure Wordless preferences here.
  */
 
-// Wordless::set_preference("css.output_style",     "compressed");
-
-// Wordless::set_preference("js.yui_compress",      false);
-// Wordless::set_preference("js.yui_munge",         false);
+if(ENV == ENV_PRODUCTION){
+	Wordless::set_preference("js.yui_compress",      true);
+	Wordless::set_preference("js.yui_munge",         true);
+}
+else {
+	Wordless::set_preference("assets.cache_enabled",  false);
+}
 
 Wordless::set_preference("css.compass_path", "/Users/Johan/.wordless_compass");
 Wordless::set_preference("js.ruby_path", "/Users/Johan/.wordless_ruby");
-
-Wordless::set_preference("assets.cache_enabled",  false);
