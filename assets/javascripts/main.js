ca207576ec1e78f6ff3be6b679963983855ffef4
Johan Brook <johan.jvb@gmail.com>
2012-04-06 20:43:09 +0200

    Fixed some vertical spacing, various responsive tweaks

---
+++ assets/javascripts/main.js
@@ -0,0 +1,43 @@
+(function() {
+  var hideAddressBar, init, preventAutoscale, scrollToContentOnSingle;
+
+  preventAutoscale = function() {
+    var viewportmeta;
+    viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+      viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+      document.body.addEventListener('gesturestart');
+    }
+    (function() {
+      return viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    });
+    return false;
+  };
+
+  hideAddressBar = function() {
+    return setTimeout(function() {
+      return scrollTo(0, 1);
+    }, 100);
+  };
+
+  scrollToContentOnSingle = function() {
+    var header_height;
+    if (document.body.className.match(/single/i)) {
+      header_height = $('[role="complementary"]').get(0).getBoundingClientRect().height;
+      return scrollTo(0, header_height);
+    }
+  };
+
+  init = function() {
+    if (navigator.userAgent.match(/iPhone/i || navigator.userAgent.match(/iPad/i))) {
+      preventAutoscale();
+      hideAddressBar();
+      return scrollToContentOnSingle();
+    }
+  };
+
+  $(document).ready(function() {
+    return init;
+  });
+
+}).call(this);
